188ccbb215e16edfccf02dbb42c1d5b9b1180802
DESKTOP-NS1P2R3\joses <josemaria@nubimetrics.com>
2023-02-17 00:48:17 -0300

    integracion de mongodb y pruebas CRUD

--- main.go
+++ main.go
@@ -1,13 +1,37 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"os"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 func main() {
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "3000"
+	}
+
+
 	app:= fiber.New()
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017/gomongodb")) //  conecto base de datos
+
+	if err != nil {
+		panic(err) //acabo la ejecucion del programa y me muestra el error
+	}
+
+	coll := client.Database("gomongodb").Collection("users")
+	coll.InsertOne(context.TODO(), bson.D{{
+		Key: "name",
+		Value:"jose",
+	}})
 
 	app.Use(cors.New())
 
@@ -16,6 +40,12 @@ func main() {
 	app.Get("/users", func(c *fiber.Ctx ) error {
 		return c.JSON(&fiber.Map{
 			"data": "usuarios desde el backend",
+		})
+	})
+
+	app.Post("/users", func(c *fiber.Ctx) error {
+		return c.JSON(&fiber.Map{
+			"data": "creando usuario",
 		})
 	})
 
--- veterinary-client/src/App.jsx
+++ veterinary-client/src/App.jsx
@@ -1,7 +1,9 @@
-
+import React, {useState} from 'react';
 import './App.css'
 
 function App() {
+
+  const [name, setName] = useState("")
 
   const handlePetitions = async() => {
     const response = await fetch('http://127.0.0.1:3000/users')
@@ -10,10 +12,27 @@ function App() {
     console.log(data)
   }
 
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const response = await fetch('http://127.0.0.1:3000/users', {
+      method: "POST",
+      body: JSON.stringify({name})
+    })
+
+    const data = await response.json()
+    console.log(data)
+
+  }
+
   return (
     <div className="App">
      <h1>Hello world </h1> 
       <button onClick={handlePetitions}>Obtener data</button>
+      <form onSubmit={handleSubmit}>
+        <input type="name" placeholder="coloca tu nombre" value={name} onChange={(e) => setName(e.target.value)}/>
+        <button type="submit">Guardar</button>
+
+      </form>
     </div>
   )
 }
